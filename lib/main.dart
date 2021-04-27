c375b2618fb2e31aed7a9bab32a5e01444977c98
Laurent-c4 <laurentjuma.c4@gmail.com>
2021-04-27 06:45:31 +0300

    Features: Implement theme toggle and Firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ import 'package:nuru_clone_app/provider/theme_provider.dart';
 import 'package:nuru_clone_app/services/authentication_service.dart';
 import 'package:provider/provider.dart';
 
-Future main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   /// Specifies the set of orientations the application interface can
   /// be displayed in.
-  await SystemChrome.setPreferredOrientations([
+  SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
@@ -29,48 +29,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider(
-    create: (context) => ThemeProvider(),
-    builder: (context, _) {
-      final themeProvider = Provider.of<ThemeProvider>(context);
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
 
-      return FutureBuilder(
-        // Initialize FlutterFire:
-        future: _initialization,
-        builder: (context, snapshot) {
-          // Check for errors
-          if (snapshot.hasError) {
-            return Center(child: Text("Oops"));
-          }
+          return FutureBuilder(
+            // Initialize FlutterFire:
+            future: _initialization,
+            builder: (context, snapshot) {
+              // Check for errors
+              if (snapshot.hasError) {
+                return Center(child: Text("Oops"));
+              }
 
-          // Once complete, show your application
-          if (snapshot.connectionState == ConnectionState.done) {
-            return MultiProvider(
-              providers: [
-                Provider<AuthenticationService>(
-                    create: (_) =>
-                        AuthenticationService(FirebaseAuth.instance)),
-                StreamProvider(
-                  create: (context) => context
-                      .read<AuthenticationService>()
-                      .authStateChanges,
-                )
-              ],
-              child: MaterialApp(
-                title: title,
-                themeMode: themeProvider.themeMode,
-                theme: AppThemes.lightTheme,
-                darkTheme: AppThemes.darkTheme,
-                home: AuthenticationWrapper(),
-              ),
-            );
-          }
+              // Once complete, show your application
+              if (snapshot.connectionState == ConnectionState.done) {
+                return MultiProvider(
+                  providers: [
+                    Provider<AuthenticationService>(
+                        create: (_) =>
+                            AuthenticationService(FirebaseAuth.instance)),
+                    StreamProvider(
+                      create: (context) => context
+                          .read<AuthenticationService>()
+                          .authStateChanges,
+                    )
+                  ],
+                  child: MaterialApp(
+                    title: title,
+                    themeMode: themeProvider.themeMode,
+                    theme: AppThemes.lightTheme,
+                    darkTheme: AppThemes.darkTheme,
+                    home: AuthenticationWrapper(),
+                  ),
+                );
+              }
 
-          // Otherwise, show something whilst waiting for initialization to complete
-          return Center(child: Text("Loading"));
+              // Otherwise, show something whilst waiting for initialization to complete
+              return Center(child: Text("Loading"));
+            },
+          );
         },
       );
-    },
-  );
 }
 
 class AuthenticationWrapper extends StatelessWidget {
@@ -78,7 +78,7 @@ class AuthenticationWrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     final firebaseUser = context.watch<User>();
 
-    if(firebaseUser != null) {
+    if (firebaseUser != null) {
       return HomePage();
     }
     return LoginPage();
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,46 +8,46 @@ import 'package:nuru_clone_app/provider/theme_provider.dart';
 import '../widgets/navigationbar_widget.dart';
 
 class HomePage extends StatelessWidget {
-  // @override
-  // Widget build(BuildContext context) => Scaffold(
-  //       appBar: AppBar(
-  //         iconTheme: Theme.of(context).iconTheme,
-  //         backgroundColor: Colors.transparent,
-  //         leading: Icon(Icons.menu),
-  //         title: Text(MyApp.title),
-  //         elevation: 0,
-  //         actions: [
-  //           ChangeThemeButtonWidget(),
-  //         ],
-  //       ),
-  //       body: ProfileWidget(),
-  //       extendBody: true,
-  //       bottomNavigationBar: NavigationBarWidget(),
-  //     );
-
   @override
-  Widget build(BuildContext context) {
-    final text = Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark
-        ? 'This is a demonstration of Dark Theme'
-        : 'This is a demonstration of Light Theme';
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: Text(MyApp.title),
-        actions: [
-          ChangeThemeButtonWidget(),
-        ],
-      ),
-      body: Center(
-        child: Text(
-          'Hello $text!',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-          ),
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          iconTheme: Theme.of(context).iconTheme,
+          backgroundColor: Colors.transparent,
+          leading: Icon(Icons.menu),
+          title: Text(MyApp.title),
+          elevation: 0,
+          actions: [
+            ChangeThemeButtonWidget(),
+          ],
         ),
-      ),
-    );
-  }
+        body: ProfileWidget(),
+        extendBody: true,
+        bottomNavigationBar: NavigationBarWidget(),
+      );
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   final text = Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark
+  //       ? 'This is a demonstration of Dark Theme'
+  //       : 'This is a demonstration of Light Theme';
+  //
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: Colors.orange,
+  //       title: Text(MyApp.title),
+  //       actions: [
+  //         ChangeThemeButtonWidget(),
+  //       ],
+  //     ),
+  //     body: Center(
+  //       child: Text(
+  //         'Hello $text!',
+  //         style: TextStyle(
+  //           fontSize: 32,
+  //           fontWeight: FontWeight.bold,
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
